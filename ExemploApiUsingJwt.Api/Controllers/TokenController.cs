792c1927e3f18d6bb3be5f0b1d3d0eeba76ac81b
Pedro Henrique de Amorim <pedrohenriquelima@uninove.edu.br>
2020-03-29 23:59:00 -0300

    Initial commit

---
+++ ExemploApiUsingJwt.Api/Controllers/TokenController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ExemploApiUsingJwt.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ExemploApiUsingJwt.Api.Controllers
+{
+    public class TokenController : Controller
+    {
+        [HttpPost]
+        [Route("api/token/create")]
+        public IActionResult Create([FromBody]LoginViewModel model)
+        {
+            if (model.Email == "teste@gmail.com" && model.Password == "numsey")
+            {
+                return new ObjectResult(GenerateToken(model.Email));
+            }
+            return BadRequest();
+        }
+
+        private string GenerateToken(string email)
+        {
+            var claims = new Claim[]
+            {
+                //define as claims 
+                new Claim(ClaimTypes.Name, email),
+                new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString()),
+            };
+
+            //gera o token
+            SymmetricSecurityKey symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("senhasupersecretaparaauth"));
+            SigningCredentials signingCredential = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+            JwtHeader jwtHeader = new JwtHeader(signingCredential);
+            JwtPayload jwtPayload = new JwtPayload(claims);
+            JwtSecurityToken token = new JwtSecurityToken(jwtHeader, jwtPayload);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ ExemploApiUsingJwt.Api/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ExemploApiUsingJwt.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAuthentication()
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("senhasupersecretaparaauth")),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(5)
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
